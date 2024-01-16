9eaaa9c6507e919fbe4e8f22710802aa7a8189d8
pushpakrawat <pushpak.rawat@outlook.com>
2024-01-17 00:05:57 +0530

    looks perfect

---
+++ Google Signin/GoogleSignInButton.js
@@ -0,0 +1,40 @@
+// GoogleSignInButton.js
+
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const GoogleSignInButton = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../assets/google-logo.png')}  // Replace with the actual path to your Google logo image
+      />
+      <Text style={styles.text}>Sign in with Google</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    width: "80%",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white', // Google's blue color
+    padding: 10,
+    borderRadius: 50,
+  },
+  logo: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  text: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+});
+
+export default GoogleSignInButton;
---
+++ Google Signin/googleSignin.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import GoogleSignInButton from './GoogleSignInButton';
+
+GoogleSignin.configure({
+  webClientId: '46173601315-jbjno2gmo29co8qpqgvqi0fru34qv0ud.apps.googleusercontent.com', // Replace with your web client ID
+});
+
+async function onGoogleButtonPress() {
+  try {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  } catch (error) {
+    console.error('Google Sign-In Error:', error);
+    throw error;
+  }
+}
+
+export function GoogleSignIn() {
+  const handleGoogleSignIn = async () => {
+    try {
+      await onGoogleButtonPress();
+      console.log('Signed in with Google!');
+    } catch (error) {
+      console.error('Error signing in with Google:', error);
+      // Handle error as needed
+    }
+  };
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={handleGoogleSignIn}>
+      <GoogleSignInButton/>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff', // Set background color as per your requirement
+  },
+  centeredContent: {
+    alignItems: 'center',
+  },
+});
+
